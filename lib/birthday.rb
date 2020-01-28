59979c166c43674cedfe65e6d837e081950be032
Micah Banschick <micha.ban@gmail.com>
2020-01-28 17:22:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,10 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  i = 0
+  while i < birthday_kids.length
+    puts "Happy Birthday #{birthday_kids[i}! You are now #{birthday_kids[i]} years old!"
+  
 end
 
 
