800b2b9d3e42eaa8ce23f234dcd6d6fdf1be96e6
Micah Banschick <micha.ban@gmail.com>
2020-01-28 17:26:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,6 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday
+  birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
